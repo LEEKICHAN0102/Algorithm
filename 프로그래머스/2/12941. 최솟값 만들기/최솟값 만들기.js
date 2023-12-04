b812a8fe0c07c68bf0eed4912ddb9f2ca5a8f98d
KICHAN LEE <112845155+LEEKICHAN0102@users.noreply.github.com>
2023-12-04 12:51:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.65 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,7 @@
 function solution(A,B){
     let sum = 0;
-   const aArr = A.slice().sort((a, b) => a - b); // 오름차순 정렬
-    const bArr = B.slice().sort((a, b) => b - a); // 내림차순 정렬
+   const aArr = A.sort((a, b) => a - b); // 오름차순 정렬
+    const bArr = B.sort((a, b) => b - a); // 내림차순 정렬
     
     for(let i=0; i<A.length; i++){
         sum += aArr[i] * bArr[i];
