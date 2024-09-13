b1fd8a29fa58779c8f16de6923db4433764b7bdf
KICHAN LEE <112845155+LEEKICHAN0102@users.noreply.github.com>
2024-09-13 23:15:51 +0900

    [level 2] Title: H-Index, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+
+    let h = 0;
+    while (h < citations.length && citations[h] > h) {
+        h++;
+    }
+
+    return h;
+}
